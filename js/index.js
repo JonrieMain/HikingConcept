f72957102862d9d42dc7c9e1ca7e8d246659b4dc
Jonrie Morales Badiang <jonriemain@gmail.com>
2022-06-24 18:10:43 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const header = document.querySelector('#header');
+const bars = document.querySelector('#bars');
+
+bars.addEventListener('click', ()=>{
+	if (bars.classList.contains('fa-bars')) {
+		bars.classList.remove('fa-bars')
+		bars.classList.add('fa-x')
+		header.style.left="0";
+		bars.style.transform="rotate(180deg)";
+	}else{
+		bars.classList.add('fa-bars')
+		bars.classList.remove('fa-x')
+		header.style.left="-100%";
+		bars.style.transform="rotate(-180deg)";
+	}
+})
+
+/* close the nav when <a> is onClick*/
+var ulChild = document.querySelectorAll(".a");
+	ulChild.forEach((btn)=>{
+		btn.addEventListener('click', ()=>{
+				bars.classList.add('fa-bars')
+				bars.classList.remove('fa-x')
+				header.style.left="-100%";
+		})
+	})
